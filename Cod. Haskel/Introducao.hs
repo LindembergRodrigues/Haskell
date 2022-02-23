170e51d1bc5e4b96e4a1d227df5d0f4458da24d5
Lindemberg Barbosa <bergfla2010@hotmail.com>
2022-02-23 15:28:45 -0300

    alterações básicas

--- Cod. Haskel/Introducao.hs
+++ Cod. Haskel/Introducao.hs
@@ -1,7 +1,16 @@
--- Retorna o dobro de um numero 
-dobro :: int -> int
-dobro x = x*2
+-- retorna o dobro do numero informado 
+dobro :: Int -> Int
+dobro x = x * 2
 
--- função em haskell
 
-resto 
+-- retorna o quadruplo do numero informado
+quadruplo :: Int -> Int
+quadruplo x = dobro (dobro x)
+
+-- Identifica se o numero informado é Impar/par
+isImpar :: Int -> String
+isImpar x = if (mod x 2) ==0 then "Par" else "Impar"
+
+--
+apresentar :: String -> String
+apresentar nome = nome ++ ", Bem vindo ao mundo Haskell"
